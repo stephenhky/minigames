e1c4d84b77b297c8c4fd021f6407034789d1f3a0
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-12-08 23:13:19 -0500

    modularization by making a class

---
+++ custommaze/clickmaze.py
@@ -0,0 +1,41 @@
+
+import json
+import logging
+from argparse import ArgumentParser
+
+import pygame
+
+from mazeutils.maze import Maze
+
+
+
+def play_game(maze_config):
+    maze = Maze(maze_config)
+    maze.draw()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            logging.debug(event)
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouse_x, mouse_y = event.pos
+                box_x, boy_y = maze.get_box_coordinates(mouse_x, mouse_y)
+                print('{}, {}'.format(box_x, boy_y))
+                for neighbor_x, neighbor_y in maze.valid_next_boxes(box_x, boy_y):
+                    print('neighbor: {}, {}'.format(neighbor_x, neighbor_y))
+
+
+def get_argparser():
+    argparser = ArgumentParser(description='Draw maze and click')
+    argparser.add_argument('mazejson', help='path of maze configuration JSON file')
+    return argparser
+
+
+if __name__ == '__main__':
+    args = get_argparser().parse_args()
+    maze_config = json.load(open(args.mazejson, 'r'))
+
+    pygame.init()
+    play_game(maze_config)
+    pygame.quit()
--- custommaze/mazeutils/maze.py
+++ custommaze/mazeutils/maze.py
@@ -1,9 +1,6 @@
 
-import json
 import logging
-from argparse import ArgumentParser
 
-import numpy as np
 import pygame
 
 
@@ -76,7 +73,7 @@ class Maze:
                 (rowbarrierx * self.width, (rowbarriery + 1) * self.height),
                 ((rowbarrierx + 1) * self.width, (rowbarriery + 1) * self.height)
             )
-        for [colbarrierx, colbarriery] in maze.colbarriers:
+        for [colbarrierx, colbarriery] in self.colbarriers:
             pygame.draw.line(
                 self.screen,
                 self.barrier_color,
@@ -89,34 +86,3 @@ class Maze:
 
     def get_box_coordinates(self, mouse_x, mouse_y):
         return (mouse_x // self.height), (mouse_y // self.width)
-
-
-def get_argparser():
-    argparser = ArgumentParser(description='Draw maze')
-    argparser.add_argument('mazejson', help='path of maze configuration JSON file')
-    return argparser
-
-
-if __name__ == '__main__':
-    args = get_argparser().parse_args()
-    maze_config = json.load(open(args.mazejson, 'r'))
-
-    maze = Maze(maze_config)
-
-    pygame.init()
-
-    maze.draw()
-    running = True
-    while running:
-        for event in pygame.event.get():
-            print(event)
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.MOUSEBUTTONUP:
-                mouse_x, mouse_y = event.pos
-                box_x, boy_y = maze.get_box_coordinates(mouse_x, mouse_y)
-                print('{}, {}'.format(box_x, boy_y))
-                for neighbor_x, neighbor_y in maze.valid_next_boxes(box_x, boy_y):
-                    print('neighbor: {}, {}'.format(neighbor_x, neighbor_y))
-
-    pygame.quit()
