745f84be7a081e74592c98f57e1ea4876c4c3494
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-12-22 20:13:27 -0500

    policy improvement

--- custommaze/mdputils.py
+++ custommaze/mdputils.py
@@ -1,14 +1,27 @@
 
+from functools import partial
+
 import numpy as np
+
+
+def make_dict_to_indices(iterable):
+    indexdict = {
+        key: i
+        for i, key in enumerate(iterable)
+    }
+    return indexdict
+
+
+def convert_dict_to_function(dictionary):
+    def fcn(x, dict1):
+        return dict1[x]
+    return partial(fcn, dict1=dictionary)
 
 
 def policy_evaluation(pi, P, gamma=1.0, epislon=1e-10, stateindexdict=None):
     # dictionary from pos to index i
     if stateindexdict is None:
-        stateindexdict = {
-            state: i
-            for i, state in enumerate(P)
-        }
+        stateindexdict = make_dict_to_indices(P.keys())
 
     # looping
     prev_V = np.zeros(len(P))
@@ -37,17 +50,11 @@ def policy_evaluation(pi, P, gamma=1.0, epislon=1e-10, stateindexdict=None):
 def policy_improvement(V, P, gamma=1.0, stateindexdict=None, actionindexdict=None):
     # dictionary from pos to indeces
     if stateindexdict is None:
-        stateindexdict = {
-            state: i
-            for i, state in enumerate(P)
-        }
+        stateindexdict = make_dict_to_indices(P.keys())
     # dictionary from action to indices
     actions = set([action for pos in P for action in P[pos]])
     if actionindexdict is None:
-        actionindexdict = {
-            action: i
-            for i, action in enumerate(actions)
-        }
+        actionindexdict = make_dict_to_indices(actions)
 
     # looping
     Q = np.zeros((len(P), len(actions)))
@@ -63,4 +70,9 @@ def policy_improvement(V, P, gamma=1.0, stateindexdict=None, actionindexdict=Non
                 done = policy_element['terminal']
                 Q[i, j] += prob * (reward + gamma*V[ip]*(not done))
 
-    new_pi = lambda state: {}  # TODO: new policy
+    optimal_actions_index = np.argmax(Q, axis=1)
+    new_pi = convert_dict_to_function({
+        state: actions[optimal_actions_index[i]]
+        for state, i in stateindexdict.items()
+    })
+    return new_pi
