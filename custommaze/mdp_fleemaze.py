f480d33fc942569149046c7c8bcad6f7a2109f35
Kwan-Yuet Ho <stephenhky@yahoo.com.hk>
2021-12-24 22:40:39 -0500

    success with discount factor

--- custommaze/mdp_fleemaze.py
+++ custommaze/mdp_fleemaze.py
@@ -5,6 +5,8 @@ from argparse import ArgumentParser
 from itertools import product
 
 import pygame
+import numpy as np
+from scipy.spatial.distance import euclidean
 
 from mazeutils.maze import Maze, ChessObject, initialize_probabilities_from_maze, Direction
 from mdputils import value_iteration, policy_iteration, make_state_action_dicts
@@ -13,18 +15,24 @@ from mdputils import value_iteration, policy_iteration, make_state_action_dicts
 imgfilename = 'doraemon.jpeg'
 
 
-def play_game(maze_config, approach):
+def play_game(maze_config, approach, gamma=0.75):
     # draw maze
     maze = Maze(maze_config)
     maze.draw()
 
     # initial_weights
+    maxdistance = euclidean(np.zeros(2), np.array([maze.nbrows-1, maze.nbcols-1]))
     P = initialize_probabilities_from_maze(maze)
     for x, y in product(range(maze.nbcols), range(maze.nbrows)):
         for direction in P[(x, y)]:
             if P[(x, y)][direction][0]['state'] == (maze.nbcols - 1, maze.nbrows - 1):
                 P[(x, y)][direction][0]['terminal'] = True
-                P[(x, y)][direction][0]['reward'] = 1.0
+                P[(x, y)][direction][0]['reward'] = 5.0
+            else:
+                newx, newy = P[(x, y)][direction][0]['state']
+                distance = euclidean(np.array([maze.nbrows-1, maze.nbcols-1]), np.array([newx, newy]))
+                P[(x, y)][direction][0]['reward'] = 1.0 - distance / maxdistance
+                pass
     print(P)
 
     # drawing image
@@ -34,11 +42,14 @@ def play_game(maze_config, approach):
     # calculating value function
     stateindexdict, actionindexdict = make_state_action_dicts(P)
     if approach == 'policy':
-        V, pi = policy_iteration(P, stateindexdict=stateindexdict, actionindexdict=actionindexdict)
+        V, pi = policy_iteration(P, gamma=gamma, stateindexdict=stateindexdict, actionindexdict=actionindexdict)
     elif approach == 'value':
-        V, pi = value_iteration(P, stateindexdict=stateindexdict, actionindexdict=actionindexdict)
+        V, pi = value_iteration(P, gamma=gamma, stateindexdict=stateindexdict, actionindexdict=actionindexdict)
     else:
         raise Exception('This block should be unreachable!')
+    print(V)
+    for state in stateindexdict.keys():
+        print('({}, {}): {}'.format(state[0], state[1], pi(state)))
 
     # looping
     running = True
@@ -68,6 +79,7 @@ def get_argparser():
     argparser = ArgumentParser(description='Draw maze and click')
     argparser.add_argument('mazejson', help='path of maze configuration JSON file')
     argparser.add_argument('approach', type=str, help='approach (option: "policy" for policy iteration, and "value" for value iteration)')
+    argparser.add_argument('--gamma', type=float, default=0.75, help='discount factor (default: 0.75)')
     return argparser
 
 
@@ -77,7 +89,8 @@ if __name__ == '__main__':
     approach = args.approach
     if not approach in ['policy', 'value']:
         raise ValueError('approach has to be either "policy" or "value", not {}.'.format(approach))
+    gamma = args.gamma
 
     pygame.init()
-    play_game(maze_config, approach)
+    play_game(maze_config, approach, gamma=gamma)
     pygame.quit()
